93079009251a462c0326aa39d7c64561c4f00c5c
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-09-18 19:57:41 -0300

    27.60 -  alteração para CDI do Núcleo JPAUtil

--- projetojsf/src/main/java/br/com/jpautil/JPAUtil.java
+++ projetojsf/src/main/java/br/com/jpautil/JPAUtil.java
@@ -1,24 +1,30 @@
 package br.com.jpautil;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+@ApplicationScoped
 public class JPAUtil {
 	
-	private static EntityManagerFactory factory = null;
+	private EntityManagerFactory factory = null;
 	
-	static {
+	public JPAUtil() {
 		if(factory == null) {
 			factory = Persistence.createEntityManagerFactory("projetojsf");
 		}
 	}
 	
-	public static EntityManager getEntityManager() {
+	@Produces
+	@RequestScoped
+	public EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
 	
-	public static Object getPrimaryKey(Object entitY) {
+	public Object getPrimaryKey(Object entitY) {
 		return factory.getPersistenceUnitUtil().getIdentifier(entitY);
 	}
 
